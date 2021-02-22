7f2a0b3ef7ad930355f1b81a9fda39d73998b3ff
mariafb <mariafb@users.noreply.github.com>
2021-02-22 22:52:44 +0000

    'new update'

--- watermark_pdf.py
+++ watermark_pdf.py
@@ -1,19 +1,6 @@
 import PyPDF2
 import sys
 
-inputs = sys.argv[1:]
-
-
-# def pdf_combiner(pdf_list):
-#     merger = PyPDF2.PdfFileMerger()
-#     for pdf in pdf_list:
-#         print(pdf)
-#         merger.append(pdf)
-#     merger.write('super.pdf')
-
-
-# pdf_combiner(inputs)
-
 template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
 watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
 output = PyPDF2.PdfFileWriter()
